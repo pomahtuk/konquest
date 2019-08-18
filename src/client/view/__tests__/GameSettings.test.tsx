a18f8bfc36074af358fffbc17c343325ac358b32
Ilia Loriakov <pman89@ya.ru>
2019-08-18 14:47:38 +0200

    fixing broken test

--- src/client/view/__tests__/GameSettings.test.tsx
+++ src/client/view/__tests__/GameSettings.test.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import wrapWithReduxAndStyle, { CurrentStore } from "../testHelpers/wrapWithReduxAndStyle";
 import { render, fireEvent } from "@testing-library/react";
+import { addPlayer } from "../../actions/game.actions";
+import Player from "../../../logic/Player";
 
 import GameSettings from "../GameSettings";
 
@@ -13,6 +15,10 @@ describe("<GameSettings />", (): void => {
   it("renders three <GameSettings /> component and component able to start game", (): void => {
     const { container, getByText } = render(wrapWithReduxAndStyle(<GameSettings />));
     expect(container).toBeDefined();
+
+    CurrentStore.dispatch(addPlayer(new Player("one")));
+    CurrentStore.dispatch(addPlayer(new Player("two")));
+
     const button = getByText("Start Game");
     fireEvent.click(button);
     expect(CurrentStore.getState().isStarted).toBe(true);
